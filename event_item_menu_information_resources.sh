b18dd9cc9905f92a939b8631c373e384176afd94
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-13 18:14:43 +0900

    Сайт АйтекИнфо

--- event_item_menu_information_resources.sh
+++ event_item_menu_information_resources.sh
@@ -2,3 +2,5 @@
 declare -A event_menu
 event_menu["$item_menu_information_resources"]="run_menu ${items_resources[@]}"
 event_menu["$item_menu_astra"]="firefox -new-tab https://astralinux.ru/ &"
+event_menu["$item_menu_aitekinfo_site"]="firefox -new-tab https://aitekinfo.ru/ &"
+
--- item_menu_information_resources.sh
+++ item_menu_information_resources.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #---------------init for item_menu_information_resources-------------------------------
 export item_menu_astra="Cайт Астра Линукс"
-export items_resources=("\"$item_menu_astra\"" "\"$exit_menu\"" "\"$exit_app\"" )
+export item_menu_aitekinfo_site="Сайт АйтекИнфо"
+export items_resources=("\"$item_menu_astra\"" "\"$item_menu_aitekinfo_site\"" "\"$exit_menu\"" "\"$exit_app\"" )
