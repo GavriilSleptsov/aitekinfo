287da0ccc530f88411a5dee1377be2f412b7fe84
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-13 18:39:29 +0900

    Update item_menu_information_resources.sh

--- item_menu_information_resources.sh
+++ item_menu_information_resources.sh
@@ -3,6 +3,8 @@
 export item_menu_astra="Cайт Астра Линукс"
 export item_menu_aitekinfo_site="Сайт АйтекИнфо"
 export item_menu_instruction_myoffice_text="Инструкция МойОфис Текст"
+export item_menu_instruction_myoffice_table="Инструкция МойОфис Таблица"
+export item_menu_instruction_myoffice_presentation\="Инструкция МойОфис Презентация"
 export items_resources=(
 "\"$item_menu_astra\""
 "\"$item_menu_aitekinfo_site\"" 
