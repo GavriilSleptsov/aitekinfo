e4c06e22e9a26fa60490e7d2eafc514bea4cc5f8
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-21 15:35:21 +0900

    Проги

--- event_item_menu/event_item_menu_information_install.sh
+++ event_item_menu/event_item_menu_information_install.sh
@@ -11,13 +11,11 @@ event_menu["$item_menu_install_telegram"]="install_apps"
 event_menu["$item_menu_install_viber"]="install_apps"
 event_menu["$item_menu_install_whatsapp"]="install_apps"
 event_menu["$item_menu_install_wps"]="install_apps"
-event_menu["$item_menu_install_code"]="install_apps"
 event_menu["$item_menu_install_notepadplus"]="install_apps"
 event_menu["$item_menu_install_yandex"]="install_app_repo"
 event_menu["$item_menu_install_remina"]="install_app_repo"
 event_menu["$item_menu_install_xrdp"]="install_app_repo"
 event_menu["$item_menu_install_scan"]="install_app_repo"
-event_menu["$item_menu_install_seahorse"]="install_app_repo"
 event_menu["$item_menu_install_myoffice"]="xdg-open https://coapi-myoffice.aitekinfo.ru/api/v1/public/a0eb7bb7-d4ef-4450-a0ef-f54ab1642aae/107585007/content?download=true &"
 
 
