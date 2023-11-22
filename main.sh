edaa2a46832e5c8bfe006c557ad5b07daa89f9e5
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-22 17:44:16 +0900

    MASTER-PDF-EDITOR

--- main.sh
+++ main.sh
@@ -104,29 +104,9 @@ source $path_events/event_item_menu_information_pomogator.sh
 ##########----------"Действия с AldPro"----------##########
 source $path_events/event_item_menu_information_aldpro.sh
 
-#source $path_install_functions/install_telegram.sh
+source $path_install_functions/install_telegram.sh
 
-install_app_telegram() {
-    passwd=$(zenity --password)
-    check_cancel
-    wget http://easyastra.ru/store/telegram.deb -P /home/$USER/Desktop/
-    file="/home/$USER/Desktop/telegram.deb"
-    # Установка пакета с указанием прогресса
-    zenity --progress --pulsate --title="Установка пакета" --text="Подождите, идет установка..." --auto-close &
-    (
-    # Установка пакета с использованием sudo и передачей пароля через stdin
-    echo $passwd | sudo -S apt install -f "$file" -y
-    # Получение кода завершения установки
-    exit_code=$?
-    # Проверка кода завершения и отображение соответствующего сообщения
-        if [ $exit_code -eq 0 ]; then
-            zenity --info --title="Успех" --text="Пакет успешно установлен!"
-        else
-            zenity --error --title="Ошибка" --text="Ошибка при установке пакета."
-        fi
-    ) | zenity --progress --pulsate --auto-close
-    rm /home/$USER/Desktop/telegram.deb
-}
+source $path_install_functions/install_master-pdf-editor.sh
 ###########################################################################################
 ############################## КОНЕЦ БЛОКА event_item_menu ################################
 ###########################################################################################
