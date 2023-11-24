d9689dac5c69a1ba60bfa0144e304b3bf40169c5
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-24 15:15:35 +0900

    Update install_myoffice.sh

--- install_functions/install_myoffice.sh
+++ install_functions/install_myoffice.sh
@@ -6,42 +6,27 @@ install_app_myoffice() {
     file="/home/$USER/Desktop/myoffice-standard-documents_2.6.0_amd64.deb"
 
     # Запуск wget в фоновом режиме для загрузки файла с отображением прогресса
-    wget --progress=bar:force -O "$file" "$url" 2>&1 | \
-    stdbuf -oL awk -v var="$file" '/[0-9]+%/ {gsub(/\r/, "", $2); print int($2)}' | \
-    zenity --progress --title="Загрузка файла" --text="Подождите, идет загрузка..." --percentage=0 --auto-close
-
+    wget --progress --pulsate -O "$file" "$url" 2>&1 | \
+    zenity --progress --title="Загрузка файла" --text="Подождите, идет загрузка..." --auto-close
     # Проверка кода завершения wget
     if [ $? -eq 0 ]; then
         zenity --info --title="Успех" --text="Файл успешно загружен!"
     else
         zenity --error --title="Ошибка" --text="Ошибка при загрузке файла."
-        exit 1
     fi
-    
     # Установка пакета с указанием прогресса
-    zenity --progress --title="Установка пакета" --text="Подождите, идет установка..." --percentage=0 &
-    progress=$!
-
+    zenity --progress --pulsate --title="Установка пакета" --text="Подождите, идет установка..." --auto-close &
     (
     # Установка пакета с использованием sudo и передачей пароля через stdin
     echo $passwd | sudo -S apt install -f "$file" -y
     # Получение кода завершения установки
     exit_code=$?
     # Проверка кода завершения и отображение соответствующего сообщения
-    if [ $exit_code -eq 0 ]; then
-        echo 100
-        zenity --info --title="Успех" --text="Пакет успешно установлен!"
-    else
-        echo 100
-        zenity --error --title="Ошибка" --text="Ошибка при установке пакета."
-    fi
-    ) | (
-        # Цикл для обновления значения прогресса
-        while read -r value; do
-            echo $value
-        done
-        # Завершение цикла, когда значение достигает 100
-        ) | zenity --progress --title="Установка пакета" --text="Подождите, идет установка..." --percentage=0 --auto-close
-
-    rm "$file"
+        if [ $exit_code -eq 0 ]; then
+            zenity --info --title="Успех" --text="Пакет успешно установлен!"
+        else
+            zenity --error --title="Ошибка" --text="Ошибка при установке пакета."
+        fi
+    ) | zenity --progress --pulsate --auto-close
+    rm /home/$USER/Desktop/myoffice-standard-documents_2.6.0_amd64.deb
 }
