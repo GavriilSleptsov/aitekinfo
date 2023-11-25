44a2694a72c876649da2b8e63a07d2d4ed90ffdd
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-25 16:25:55 +0900

    Update install_myoffice.sh

--- install_functions/install_myoffice.sh
+++ install_functions/install_myoffice.sh
@@ -2,7 +2,6 @@ declare -A event_menu
 install_app_myoffice() {
     passwd=$(zenity --password)
     check_cancel
-    url="https://slepsov.ru/aitekinfo/myoffice-standard-documents_2.6.0_amd64.deb"
     file="/home/$USER/Desktop/myoffice-standard-documents_2.6.0_amd64.deb"
 
 	zenity --progress --pulsate --title="Загрузка пакета" --text="Подождите, идет загрузка..." &
@@ -17,15 +16,7 @@ install_app_myoffice() {
 			exit 1
 		fi
     ) | zenity --progress --pulsate --auto-close
-
-    # Проверка кода завершения wget
-    if [ $? -eq 0 ]; then
-        zenity --info --title="Успех" --text="Файл успешно загружен!"
-    else
-        zenity --error --title="Ошибка" --text="Ошибка при загрузке файла."
-        exit 1
-    fi
-
+	
     # Установка пакета
     zenity --progress --pulsate --title="Установка пакета" --text="Подождите, идет установка..." --auto-close &
     (
