86a2e3804bc66adc45f22bc3dbc9291be66f8a22
GavriilSleptsov <killerjikey84@gmail.com>
2023-11-27 16:17:58 +0900

    Update install_armgs.sh

--- install_functions/install_armgs.sh
+++ install_functions/install_armgs.sh
@@ -8,10 +8,9 @@ install_app_armgs() {
 		check_cancel
 		zenity --auto-close &
 		(
-			echo $passwd | sudo -S mkdir /home/$USER/Desktop/armgs
-			echo $passwd | sudo -S mkdir /opt/armgs
+			echo $passwd | sudo -S mkdir /home/$USER/Desktop/armgs/
+			echo $passwd | sudo -S mkdir /opt/armgs/
 			wget https://dl.armgs.team/downloads/linux/x64/latest/armgs.tar.xz -P /home/$USER/Desktop/armgs
-			echo $passwd | sudo -S cp /home/$USER/Desktop/armgs.tar.xz /opt/armgs
 			# Проверка кода завершения wget
 			if [ $? -eq 0 ]; then
 				zenity --info --title="Успех" --text="Файл успешно загружен!"
@@ -27,7 +26,7 @@ install_app_armgs() {
 			# Установка пакета с использованием sudo и передачей пароля через stdin
 			echo $passwd | sudo -S tar -xf /home/$USER/Desktop/armgs/armgs.tar.xz 
 			echo $passwd | sudo -S rm /home/$USER/Desktop/armgs/armgs.tar.xz 
-			echo $passwd | sudo -S cp -r /home/$USER/Desktop/armgs /opt/armgs
+			echo $passwd | sudo -S mv -r /home/$USER/Desktop/armgs /opt/armgs
 			
 			# Получение кода завершения установки
 			#exit_code=$?
