251dd084d9997a906c33400c8fc04e6ba31a0422
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-03 14:49:40 +0900

    news

--- item_menu/item_menu_information_pomogator.sh
+++ item_menu/item_menu_information_pomogator.sh
@@ -3,4 +3,8 @@
 export item_menu_information_pomogator_version="Версия приложения"
 export item_menu_information_pomogator_update="Обновить на новую версию"
 export item_menu_information_pomogator_news="Нововведения в обновление"
-export item_menu_pomogator_all=("\"$item_menu_information_pomogator_version\"" "\"$item_menu_information_pomogator_update\"" "\"$item_menu_information_pomogator_news\"" "\"$exit_menu\"" "\"$exit_app\"")
+export item_menu_pomogator_all=(
+"\"$item_menu_information_pomogator_version\""
+ "\"$item_menu_information_pomogator_update\"" 
+ "\"$item_menu_information_pomogator_news\"" 
+ "\"$exit_menu\"" "\"$exit_app\"")
