9a7e37f6026c14bca534a8dbbddc8ca292ea873e
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-04 12:02:58 +0900

    Update install_notepad.sh

--- install_functions/install_notepad.sh
+++ install_functions/install_notepad.sh
@@ -13,30 +13,30 @@ install_app_notepad() {
 			# Проверка кода завершения wget
 			if [ $? -eq 0 ]; then
 				zenity --info --title="Успех" --text="Файл успешно загружен!"
+				zenity --auto-close &
+		(
+				# Установка пакета с использованием sudo и передачей пароля через stdin
+				echo $passwd | sudo -S apt install "$file" -y
+				# Получение кода завершения установки
+				exit_code=$?
+				# Проверка кода завершения и отображение соответствующего сообщения
+				if [ $exit_code -eq 0 ]; then
+					zenity --info --title="Успех" --text="Пакет успешно установлен!"
+					cp $file_path /home/$USER/Desktop
+				else
+						zenity --error --title="Ошибка" --text="Ошибка при установке пакета."
+				fi
+				) | zenity --progress --pulsate --title "Установка пакета" --text="Подождите, идет установка..." --auto-close
+				# Проверка наличия файла перед удалением
+				if [ -e "$file" ]; then
+					rm "$file"
+				fi
+				#
 			else
 				zenity --error --title="Ошибка" --text="Ошибка при загрузке файла."
-				exit 1
+				return
 			fi
 		) | zenity --progress --pulsate --title "Загрузка пакета" --text="Подождите, идет загрузка..." --auto-close
 		
-		# Установка пакета
-		zenity --auto-close &
-		(
-			# Установка пакета с использованием sudo и передачей пароля через stdin
-			echo $passwd | sudo -S apt install "$file" -y
-			# Получение кода завершения установки
-			exit_code=$?
-			# Проверка кода завершения и отображение соответствующего сообщения
-			if [ $exit_code -eq 0 ]; then
-				zenity --info --title="Успех" --text="Пакет успешно установлен!"
-				cp $file_path /home/$USER/Desktop
-			else
-				zenity --error --title="Ошибка" --text="Ошибка при установке пакета."
-			fi
-		) | zenity --progress --pulsate --title "Установка пакета" --text="Подождите, идет установка..." --auto-close
-		# Проверка наличия файла перед удалением
-		if [ -e "$file" ]; then
-			rm "$file"
-		fi
 	fi
 }
